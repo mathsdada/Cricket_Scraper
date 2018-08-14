a9b4dd8593758145014c2ffe73467b8f59457a27
vgangadh <mathsdada@gmail.com>
2018-08-14 15:11:40 +0530

    team and venue query listing

--- database/query/team.py
+++ database/query/team.py
@@ -1,3 +1,52 @@
 class Team:
     def __init__(self, db_cursor):
         self.cursor = db_cursor
+
+    def team_form(self, team_name, format, num_of_matches):
+        # Win - Loss like (W L L W W W L L). Get opposite team name as well.
+        pass
+
+    def team_match_scores(self, team_name, format, num_of_matches):
+        pass
+
+    def most_runs(self, team_squad, format, num_of_matches):
+        # batsman_name num_matches runs balls highest_score
+        # Array of above
+        pass
+
+    def most_wickets(self, team_squad, format, num_of_matches):
+        # bowler_name num_matches wickets runs
+        # Array of above
+        pass
+
+    def best_economy(self, team_squad, format, num_of_matches):
+        # bowler_name num_matches overs runs wickets economy
+        # Array of above
+        pass
+
+    def record_against_opposite_team(self, team_name, opposite_team_name, format, num_of_matches):
+        pass
+
+    def team_form_at_venue(self, team_name, venue, format, num_of_matches):
+        pass
+
+    def team_match_scores_at_venue(self, team_name, venue, format, num_of_matches):
+        pass
+
+    def most_runs_at_venue(self, team_squad, venue, format, num_of_matches):
+        # batsman_name num_matches runs balls highest_score
+        # Array of above
+        pass
+
+    def most_wickets_at_venue(self, team_squad, venue, format, num_of_matches):
+        # bowler_name num_matches wickets runs
+        # Array of above
+        pass
+
+    def best_economy_at_venue(self, team_squad, venue, format, num_of_matches):
+        # bowler_name num_matches overs runs wickets economy
+        # Array of above
+        pass
+
+    def record_against_opposite_team_at_venue(self, team_name, opposite_team_name, venue, format, num_of_matches):
+        pass
--- database/query/venue.py
+++ database/query/venue.py
@@ -1,3 +1,24 @@
 class Venue:
     def __init__(self, db_cursor):
         self.cursor = db_cursor
+
+    def match_scores(self, venue, format, num_of_matches):
+        pass
+
+    def best_batsmen(self, venue, format, num_of_matches):
+        pass
+
+    def best_bowlers(self, venue, bowler_names, format, num_of_matches):
+        pass
+
+    def wickets_per_bowling_style(self, venue, bowling_style, format, num_of_matches):
+        pass
+
+    def per_innings_avg_score(self, venue, format, num_of_matches):
+        pass
+
+    def team_batting_first_win_count(self, venue, format, number_of_matches):
+        pass
+
+    def team_batting_second_win_count(self, venue, format, number_of_matches):
+        pass
