d13b63e5afcb39d28254dbda53d939974ce2fa61
Gangadhar Vukkesala <gangadhar.nitw@gmail.com>
2018-08-28 08:24:15 +0530

    Schedule Part - 2

--- scraper/model/schedule/sched.py
+++ scraper/model/schedule/sched.py
@@ -9,7 +9,9 @@ class Schedule:
 
     def __get_schedule(self):
         soup = Common.get_soup_object("https://www.cricbuzz.com/cricket-schedule/upcoming-series/")
-        date_blocks = soup.find_all('div', {'class': 'cb-col-100 cb-col', 'ng-show': True})
-        for date_block in date_blocks:
-            if date_block.next_element.text == self.date:
-                print(date_block.next_element.text)
+        category_blocks = soup.find_all('div', {'class': 'cb-col-100 cb-col', 'ng-show': True})
+        for category_block in category_blocks:
+            if category_block.next_element.text == self.date:
+                series_blocks = category_block.find_all('div', class_='cb-col-100 cb-col')
+                for series_block in series_blocks:
+                    print(series_block.next_element.text)
