725564ae71a7bb302019d9cfe8be99d5276ccd52
Gangadhar Vukkesala <gangadhar.nitw@gmail.com>
2018-09-19 22:10:15 +0530

    Player Table Update only if role or bat or bowling style changed

--- database/schema/player.py
+++ database/schema/player.py
@@ -1,17 +1,28 @@
+from database.query.common import Common
+
+
 class Player:
     def __init__(self, cursor):
         self.cursor = cursor
 
     def insert(self, id, name, role, batting_style, bowling_style, gender):
         sql = """INSERT INTO player VALUES(%s, %s, %s, %s, %s, %s)"""
-        if not self.__check_player_id(id):
-            self.cursor.execute(sql, (id, name, role, batting_style, bowling_style, gender))
-            return True
-        return False
 
-    def __check_player_id(self, id):
+        player_data = self.__get_player_data(id)
+        if player_data is None:
+            self.cursor.execute(sql, (id, name, role, batting_style, bowling_style, gender))
+        else:
+            if (player_data['role'] != role) or (player_data['batting_style'] != batting_style) or \
+                    (player_data['bowling_style'] != bowling_style):
+                self.update(id, name, role, batting_style, bowling_style, gender)
+
+    def update(self, id, name, role, batting_style, bowling_style, gender):
+        sql = """UPDATE player SET role = %s, batting_style = %s, bowling_style = %s WHERE id = %s"""
+        self.cursor.execute(sql, (role, batting_style, bowling_style, id))
+
+    def __get_player_data(self, id):
         sql = """SELECT * FROM player WHERE player.id = %s"""
         self.cursor.execute(sql, (id,))
         if self.cursor.rowcount > 0:
-            return True
-        return False
+            return Common.extract_query_results(self.cursor)
+        return None
